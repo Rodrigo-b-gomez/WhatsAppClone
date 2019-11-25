b7fade57fddb650b088f971ed82becc3a4d45a9e
Rodrigo-b-gomez <rodrigo-b-gomez@hotmail.com>
2019-11-25 14:04:33 -0600

    Clon WhatsApp Flutter

---
+++ FlutterWhatsAppClone-master/lib/models/chat_model.dart
@@ -0,0 +1,47 @@
+class ChatModel {
+  final String name;
+  final String message;
+  final String time;
+  final String avatarUrl;
+
+  ChatModel({this.name, this.message, this.time, this.avatarUrl});
+}
+
+List<ChatModel> dummyData = [
+  new ChatModel(
+      name: "Chico papas",
+      message: "Vas a querer papas !",
+      time: "15:30",
+      avatarUrl:
+          "http://www.usanetwork.com/sites/usanetwork/files/styles/629x720/public/suits_cast_harvey.jpg?itok=fpTOeeBb"),
+  new ChatModel(
+      name: "El moikas",
+      message: "Simona la mona!",
+      time: "17:30",
+      avatarUrl:
+          "http://www.usanetwork.com/sites/usanetwork/files/styles/629x720/public/suits_cast_harvey.jpg?itok=fpTOeeBb"),
+  new ChatModel(
+      name: "Compa",
+      message: "Jalese perro !",
+      time: "5:00",
+      avatarUrl:
+          "http://www.usanetwork.com/sites/usanetwork/files/styles/629x720/public/suits_cast_harvey.jpg?itok=fpTOeeBb"),
+  new ChatModel(
+      name: "Gus",
+      message: "Que ondui!",
+      time: "10:30",
+      avatarUrl:
+          "http://www.usanetwork.com/sites/usanetwork/files/styles/629x720/public/suits_cast_harvey.jpg?itok=fpTOeeBb"),
+  new ChatModel(
+      name: "Ferras",
+      message: "Za za za!",
+      time: "12:30",
+      avatarUrl:
+          "http://www.usanetwork.com/sites/usanetwork/files/styles/629x720/public/suits_cast_harvey.jpg?itok=fpTOeeBb"),
+  new ChatModel(
+      name: "Panes",
+      message: "Comete un pan cometelo, cmetelo !",
+      time: "15:30",
+      avatarUrl:
+          "http://www.usanetwork.com/sites/usanetwork/files/styles/629x720/public/suits_cast_harvey.jpg?itok=fpTOeeBb"),
+];
